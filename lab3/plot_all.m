384e9b736da889a08fa6a53d73f9c8da76de6133
un-olk <olk.zaytsev@gmail.com>
2015-11-27 10:48:29 +0200

    Plotted the results of Fixed point iteration, Newton's method and Modified Newton's method. Receiving initial values from user-input

--- lab3/plot_all.m
+++ lab3/plot_all.m
@@ -1,15 +1,57 @@
-% Plot the functions to deduce x_init
+% prevent the previous plot from dessapearing
+% when the next one comes forward
+hold on;
 
-x1_init = 1.5;
-x2_init = 1.5;
-prec = 0.001;
+markersize = 10;
+linewidth = 2;
 
-% [x1, x2, steps] = fixed_point_iter(x1_init, x2_init, prec);
-% disp(x1);
-% disp(x2);
-% disp(steps);
+a = 0;
+b = 10;
+step = (b - a) / 100;
 
-[x1, x2, steps] = newton_mod(x1_init, x2_init, prec);
-disp(x1);
-disp(x2);
-disp(steps);
+% plot given functions
+interval = [a:step:b];
+
+plot(interval, func1(interval, interval), 'r', 'linewidth', linewidth);
+plot(interval, func2(interval, interval), 'b', 'linewidth', linewidth);
+
+% get values to plot
+satisfied = false;
+while !satisfied
+	printf('Input two initial x-values from the interval [%0.1f; %0.1f].\n',
+		a, b);
+	printf('Hint: Use Octave vector format.\n');
+	x_init = input('');
+	printf('\n');
+
+	satisfied = true;
+
+	for i = 1:2
+		if (x_init(i) < a || x_init(i) > b)
+			printf('Your values exceed the interval. Try again\n\n');
+			satisfied = false;
+			break;
+		endif;
+	end;
+end;
+
+% plot requested values
+plot(x_init(1), func1(x_init(1), x_init(2)), 'r', 'markersize', markersize);
+plot(x_init(2), func2(x_init(2), x_init(2)), 'b', 'markersize', markersize);
+
+prec = 0.000001;
+
+[x1, x2, steps] = fixed_point_iter(x_init(1), x_init(2), prec);
+printf('Fixed point iteration:\n');
+printf('\tx = [%f; %f]\n', x1, x2);
+printf('\tSteps: %d\n\n', steps);
+
+[x1, x2, steps] = newton(x_init(1), x_init(2), prec);
+printf("Newton's method:\n");
+printf('\tx = [%f; %f]\n', x1, x2);
+printf('\tSteps: %d\n\n', steps);
+
+[x1, x2, steps] = newton_mod(x_init(1), x_init(2), prec);
+printf("Modified Newton's method:\n");
+printf('\tx = [%f; %f]\n', x1, x2);
+printf('\tSteps: %d\n\n', steps);
